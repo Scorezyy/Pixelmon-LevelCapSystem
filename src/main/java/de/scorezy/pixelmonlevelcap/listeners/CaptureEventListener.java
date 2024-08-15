25ddc3f5195e7a727a6e76a3f7a34d0566364d0d
Scorezyy <Jxstn.gaming@gmail.com>
2024-08-15 21:51:30 +0200

    Bug Fix

--- src/main/java/de/scorezy/pixelmonlevelcap/listeners/CaptureEventListener.java
+++ src/main/java/de/scorezy/pixelmonlevelcap/listeners/CaptureEventListener.java
@@ -3,6 +3,7 @@ package de.scorezy.pixelmonlevelcap.listeners;
 import com.pixelmonmod.pixelmon.api.events.CaptureEvent;
 import de.scorezy.pixelmonlevelcap.utils.BadgeUtils;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -14,9 +15,50 @@ public class CaptureEventListener {
         int pokemonLevel = event.getPokemon().getLvl().getPokemonLevel();
         int maxLevel = BadgeUtils.getMaxLevelForPlayer(player);
 
-        if (pokemonLevel > maxLevel) {
-            event.setCanceled(true);
-            player.sendMessage(new StringTextComponent("§cDieses §ePokémon §cist zu stark für dich um es zu Fangen!"), player.getUUID());
+        if (isCaptureBlocked(pokemonLevel, maxLevel)) {
+            handleCaptureRestriction(event, player);
         }
+    }
+
+    private boolean isCaptureBlocked(int pokemonLevel, int maxLevel) {
+        return pokemonLevel > maxLevel;
+    }
+
+    private void handleCaptureRestriction(CaptureEvent.StartCapture event, ServerPlayerEntity player) {
+        cancelEvent(event, player);
+        returnBallToPlayer(event, player);
+    }
+
+    private void cancelEvent(CaptureEvent.StartCapture event, ServerPlayerEntity player) {
+        event.setCanceled(true);
+        player.sendMessage(new StringTextComponent("§cDieses §ePokémon §cist zu stark für dich um es zu Fangen!"), player.getUUID());
+    }
+
+    private void returnBallToPlayer(CaptureEvent.StartCapture event, ServerPlayerEntity player) {
+        ItemStack ballStack = event.getPokeBall().getBallType().getBallItem();
+        if (!ballStack.isEmpty()) {
+            addBallToInventory(player, ballStack);
+        }
+    }
+
+    private void addBallToInventory(ServerPlayerEntity player, ItemStack ballStack) {
+        for (int i = 0; i < player.inventory.getContainerSize(); i++) {
+            ItemStack slotStack = player.inventory.getItem(i);
+            if (slotStack.isEmpty()) {
+                player.inventory.setItem(i, ballStack.copy());
+                return;
+            }
+            if (slotStack.getItem() == ballStack.getItem()) {
+                addToStack(slotStack, ballStack);
+                if (ballStack.isEmpty()) return;
+            }
+        }
+    }
+
+    private void addToStack(ItemStack slotStack, ItemStack ballStack) {
+        int space = slotStack.getMaxStackSize() - slotStack.getCount();
+        int amountToAdd = Math.min(space, ballStack.getCount());
+        slotStack.grow(amountToAdd);
+        ballStack.shrink(amountToAdd);
     }
 }
