28b32a4e4dea398e444212a9112e7e91ef642a9d
wujichen158 <1169083089@qq.com>
2024-08-24 12:04:49 +0800

    feat(level up listener): update player object NPE checking ways

--- src/main/java/de/scorezy/pixelmonlevelcap/listeners/LevelUpEventListener.java
+++ src/main/java/de/scorezy/pixelmonlevelcap/listeners/LevelUpEventListener.java
@@ -7,38 +7,45 @@ import de.scorezy.pixelmonlevelcap.utils.BadgeUtils;
 import de.scorezy.pixelmonlevelcap.utils.ConfigLoader;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.Optional;
 
 public class LevelUpEventListener {
 
     @SubscribeEvent
     public void onPokemonLevelUp(LevelUpEvent.Pre event) {
-        ServerPlayerEntity player = event.getPlayer();
-        int pokemonLevel = event.getAfterLevel();
-        int maxLevel = BadgeUtils.getMaxLevelForPlayer(player);
+        Optional.ofNullable(event.getPlayer()).ifPresent(serverPlayer -> {
+            int pokemonLevel = event.getAfterLevel();
+            int maxLevel = BadgeUtils.getMaxLevelForPlayer(serverPlayer);
 
-        if (pokemonLevel > maxLevel) {
-            event.setCanceled(true);
-            String message = ConfigLoader.getLevelBlockedMessage();
-            player.sendMessage(new StringTextComponent(message), player.getUUID());
-        }
+            if (pokemonLevel > maxLevel) {
+                event.setCanceled(true);
+                serverPlayer.sendMessage(
+                        new TranslationTextComponent("pixelmonlevelcap.capped_msg.level_blocked"),
+                        serverPlayer.getUUID());
+            }
+        });
     }
 
     @SubscribeEvent
     public void onExperienceGain(ExperienceGainEvent event) {
-        ServerPlayerEntity player = findPlayerForPokemon(event.pokemon.getPokemon());
-        int currentLevel = event.pokemon.getPokemonLevel();
-        int maxLevel = BadgeUtils.getMaxLevelForPlayer(player);
+        Optional.ofNullable(findPlayerForPokemon(event.pokemon.getPokemon())).ifPresent(serverPlayer -> {
+            int currentLevel = event.pokemon.getPokemonLevel();
+            int maxLevel = BadgeUtils.getMaxLevelForPlayer(serverPlayer);
 
-        if (player != null && currentLevel >= maxLevel) {
-            event.setExperience(0);
-            String message = ConfigLoader.getMaxLevelReachedMessage();
-            player.sendMessage(new StringTextComponent(message), player.getUUID());
-        }
+            if (currentLevel >= maxLevel) {
+                event.setExperience(0);
+                serverPlayer.sendMessage(
+                        new TranslationTextComponent("pixelmonlevelcap.capped_msg.max_level_reached"),
+                        serverPlayer.getUUID());
+            }
+        });
     }
 
     private ServerPlayerEntity findPlayerForPokemon(Pokemon pokemon) {
-        //Platzhalter ;)
+        //Immer noch ein Platzhalter ;)
         return null;
     }
 }
