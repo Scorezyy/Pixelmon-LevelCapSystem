31ca3a01fc66d8844193501c6ca2ccddca6646ea
wujichen158 <1169083089@qq.com>
2024-08-25 19:13:22 +0800

    feat(config): added a comment to the capped levels config entry

--- src/main/java/de/scorezy/pixelmonlevelcap/config/PLCConfig.java
+++ src/main/java/de/scorezy/pixelmonlevelcap/config/PLCConfig.java
@@ -4,6 +4,7 @@ import com.pixelmonmod.pixelmon.api.config.api.data.ConfigPath;
 import com.pixelmonmod.pixelmon.api.config.api.yaml.AbstractYamlConfig;
 import de.scorezy.pixelmonlevelcap.lib.Reference;
 import info.pixelmon.repack.org.spongepowered.objectmapping.ConfigSerializable;
+import info.pixelmon.repack.org.spongepowered.objectmapping.meta.Comment;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +18,7 @@ public class PLCConfig extends AbstractYamlConfig {
     /**
      * These are the default values that will be used if this entry not created.
      */
+    @Comment("Listed levels will be capped based on the number of badges.")
     private List<Integer> cappedLevels = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 100);
 
     public List<Integer> getCappedLevels() {
