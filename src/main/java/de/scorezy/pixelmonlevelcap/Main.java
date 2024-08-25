8fa897a6b216f6ccd313d4806653473badfcd7e3
wujichen158 <1169083089@qq.com>
2024-08-25 10:29:35 +0800

    feat(logger): add a logger for this mod

--- src/main/java/de/scorezy/pixelmonlevelcap/Main.java
+++ src/main/java/de/scorezy/pixelmonlevelcap/Main.java
@@ -13,9 +13,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Reference.MOD_ID)
 public class Main {
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public Main() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
