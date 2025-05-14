8ced310262653e0793fc877e257a82cbe2ffcbcc
ChefPC <Jxstn.privat@gmail.com>
2025-05-14 19:25:28 +0200

    *EVERYBODY SEE MY MISSTAKE MUAHAHAHA

--- src/main/java/de/scorezy/pixelmonlevelcap/listeners/TradeEventListener.java
+++ src/main/java/de/scorezy/pixelmonlevelcap/listeners/TradeEventListener.java
@@ -5,6 +5,7 @@ import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import de.scorezy.pixelmonlevelcap.utils.BadgeUtils;
 import de.scorezy.pixelmonlevelcap.utils.ConfigLoader;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.Util;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -14,46 +15,38 @@ public class TradeEventListener {
     public void onPixelmonTrade(PixelmonTradeEvent.Pre event) {
         ServerPlayerEntity player1 = (ServerPlayerEntity) event.getPlayer1();
         ServerPlayerEntity player2 = (ServerPlayerEntity) event.getPlayer2();
-        Pokemon pokemon1 = event.getPokemon1();
-        Pokemon pokemon2 = event.getPokemon2();
-        boolean shouldCancel = false;
+        Pokemon poke1 = event.getPokemon1();
+        Pokemon poke2 = event.getPokemon2();
 
-        if (pokemon1 != null && exceedsMaxLevel(pokemon1, player1)) {
-            shouldCancel = true;
-            player1.sendMessage(
-                    new StringTextComponent(ConfigLoader.getTradeBlockedMessage()),
-                    player1.getUUID()
-            );
+        if (checkAndNotify(poke1, player1, player2)) {
+            event.setCanceled(true);
+            return;
         }
-        if (pokemon2 != null && exceedsMaxLevel(pokemon2, player2)) {
-            shouldCancel = true;
-            player2.sendMessage(
-                    new StringTextComponent(ConfigLoader.getTradeBlockedMessage()),
-                    player2.getUUID()
-            );
-        }
-
-        if (pokemon1 != null && exceedsMaxLevel(pokemon1, player2)) {
-            shouldCancel = true;
-            player2.sendMessage(
-                    new StringTextComponent(ConfigLoader.getTradeBlockedPartnerMessage()),
-                    player2.getUUID()
-            );
-        }
-        if (pokemon2 != null && exceedsMaxLevel(pokemon2, player1)) {
-            shouldCancel = true;
-            player1.sendMessage(
-                    new StringTextComponent(ConfigLoader.getTradeBlockedPartnerMessage()),
-                    player1.getUUID()
-            );
-        }
-
-        if (shouldCancel) {
+        if (checkAndNotify(poke2, player2, player1)) {
             event.setCanceled(true);
         }
     }
 
-    private boolean exceedsMaxLevel(Pokemon pokemon, ServerPlayerEntity player) {
-        return pokemon.getPokemonLevel() > BadgeUtils.getMaxLevelForPlayer(player);
+    private boolean checkAndNotify(Pokemon pokemon, ServerPlayerEntity owner, ServerPlayerEntity partner) {
+        if (pokemon == null) {
+            return false;
+        }
+
+        int level      = pokemon.getPokemonLevel();
+        int ownerCap   = BadgeUtils.getMaxLevelForPlayer(owner);
+        int partnerCap = BadgeUtils.getMaxLevelForPlayer(partner);
+
+        if (level > ownerCap) {
+            owner.sendMessage(new StringTextComponent(ConfigLoader.getTradeBlockedMessage()), Util.NIL_UUID);
+            return true;
+        }
+
+        if (level > partnerCap) {
+            partner.sendMessage(new StringTextComponent(ConfigLoader.getTradeBlockedMessage()), Util.NIL_UUID);
+            owner.sendMessage(new StringTextComponent(ConfigLoader.getTradeBlockedPartnerMessage()), Util.NIL_UUID);
+            return true;
+        }
+
+        return false;
     }
 }
